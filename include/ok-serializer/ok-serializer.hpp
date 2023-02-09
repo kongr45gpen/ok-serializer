d801bacab9b62e2b3821839ec05b3e5a386381a6
kongr45gpen <electrovesta@gmail.com>
2023-02-10 00:20:13 +0100

    ci

--- include/ok-serializer/ok-serializer.hpp
+++ include/ok-serializer/ok-serializer.hpp
@@ -2,40 +2,38 @@
 
 #include <tuple>
 
-#include "impl_types.h"
 #include "impl_outputs.h"
+#include "impl_types.h"
 
 namespace okser {
-    template<typename T1, typename T2>
-    struct mypair {
-        using SerializerType = T1;
-        using ValueType = T2;
-        T2 value;
-    };
+template <typename T1, typename T2> struct mypair {
+  using SerializerType = T1;
+  using ValueType = T2;
+  T2 value;
+};
 
-    namespace internal {
-        template<class Pair, class Output>
-        constexpr void serialize_one(Pair p, Output&& o) {
-            Pair::SerializerType::serialize(p.value, o);
-        }
-    }
-
-    template<class... Types>
-    class bundle {
-    public:
-        template<class Output, typename... Values>
-        constexpr static void serialize(Output&& output, Values... values) {
-            std::tuple<mypair<Types, Values>...> typeValues{values...};
-
-            std::apply([&output](auto &&... v) {
-                ((internal::serialize_one(v, output)), ...);
-            }, typeValues);
-        }
-    };
-
-    template<class Bundle, class Output, typename... Values>
-    constexpr void serialize(Output&& output, Values... values) {
-        return Bundle::serialize(output, values...);
-    }
-
+namespace internal {
+template <class Pair, class Output>
+constexpr void serialize_one(Pair p, Output &&o) {
+  Pair::SerializerType::serialize(p.value, o);
 }
+} // namespace internal
+
+template <class... Types> class bundle {
+public:
+  template <class Output, typename... Values>
+  constexpr static void serialize(Output &&output, Values... values) {
+    std::tuple<mypair<Types, Values>...> typeValues{values...};
+
+    std::apply(
+        [&output](auto &&...v) { ((internal::serialize_one(v, output)), ...); },
+        typeValues);
+  }
+};
+
+template <class Bundle, class Output, typename... Values>
+constexpr void serialize(Output &&output, Values... values) {
+  return Bundle::serialize(output, values...);
+}
+
+} // namespace okser
--- test/source/ok-serializer_test.cpp
+++ test/source/ok-serializer_test.cpp
@@ -3,160 +3,127 @@
 
 using Catch::Matchers::Equals;
 
-TEST_CASE( "uint encoding" ) {
-    SECTION("uint8_t") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<1>>>(
-                okser::out::stdstring{out},
-                124
-                );
-        REQUIRE(out[0] == 0x7C);
-    }
+TEST_CASE("uint encoding") {
+  SECTION("uint8_t") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<1>>>(okser::out::stdstring{out},
+                                                    124);
+    REQUIRE(out[0] == 0x7C);
+  }
 
-    SECTION("uint16_t big-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<2, okser::end::be>>>(
-                okser::out::stdstring{out},
-                17328
-            );
-        CHECK_THAT(out, Equals("\x43\xB0"));
-    }
+  SECTION("uint16_t big-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<2, okser::end::be>>>(
+        okser::out::stdstring{out}, 17328);
+    CHECK_THAT(out, Equals("\x43\xB0"));
+  }
 
-    SECTION("uint16_t little-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<2, okser::end::le>>>(
-                okser::out::stdstring{out},
-                17328
-            );
-        CHECK_THAT(out, Equals("\xB0\x43"));
-    }
-    SECTION("uint24_t big-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<3, okser::end::be>>>(
-                okser::out::stdstring{out},
-                359352
-            );
-        CHECK_THAT(out, Equals("\x05\x7B\xB8"));
-    }
+  SECTION("uint16_t little-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<2, okser::end::le>>>(
+        okser::out::stdstring{out}, 17328);
+    CHECK_THAT(out, Equals("\xB0\x43"));
+  }
+  SECTION("uint24_t big-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<3, okser::end::be>>>(
+        okser::out::stdstring{out}, 359352);
+    CHECK_THAT(out, Equals("\x05\x7B\xB8"));
+  }
 
-    SECTION("uint24_t little-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<3, okser::end::le>>>(
-                okser::out::stdstring{out},
-                359352
-            );
-        CHECK_THAT(out, Equals("\xB8\x7B\x05"));
-    }
+  SECTION("uint24_t little-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<3, okser::end::le>>>(
+        okser::out::stdstring{out}, 359352);
+    CHECK_THAT(out, Equals("\xB8\x7B\x05"));
+  }
 }
 
-TEST_CASE( "sint encoding" ) {
-    SECTION("sint8_t") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::sint<1>>>(
-                okser::out::stdstring{out},
-                -99
-                );
-        CHECK_THAT(out, Equals("\x9D"));
-    }
+TEST_CASE("sint encoding") {
+  SECTION("sint8_t") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::sint<1>>>(okser::out::stdstring{out},
+                                                    -99);
+    CHECK_THAT(out, Equals("\x9D"));
+  }
 
-    SECTION("sint16_t big-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::sint<2, okser::end::be>>>(
-                okser::out::stdstring{out},
-                -31315
-            );
-        CHECK_THAT(out, Equals("\x85\xAD"));
-    }
+  SECTION("sint16_t big-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::sint<2, okser::end::be>>>(
+        okser::out::stdstring{out}, -31315);
+    CHECK_THAT(out, Equals("\x85\xAD"));
+  }
 
-    SECTION("sint16_t little-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::sint<2, okser::end::le>>>(
-                okser::out::stdstring{out},
-                -31315
-            );
-        CHECK_THAT(out, Equals("\xAD\x85"));
-    }
-    SECTION("sint24_t big-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<3, okser::end::be>>>(
-                okser::out::stdstring{out},
-                -7016456
-            );
-        CHECK_THAT(out, Equals("\x94\xEF\xF8"));
-    }
+  SECTION("sint16_t little-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::sint<2, okser::end::le>>>(
+        okser::out::stdstring{out}, -31315);
+    CHECK_THAT(out, Equals("\xAD\x85"));
+  }
+  SECTION("sint24_t big-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<3, okser::end::be>>>(
+        okser::out::stdstring{out}, -7016456);
+    CHECK_THAT(out, Equals("\x94\xEF\xF8"));
+  }
 
-    SECTION("sint24_t little-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::uint<3, okser::end::le>>>(
-                okser::out::stdstring{out},
-                -7016456
-            );
-        CHECK_THAT(out, Equals("\xF8\xEF\x94"));
-    }
+  SECTION("sint24_t little-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::uint<3, okser::end::le>>>(
+        okser::out::stdstring{out}, -7016456);
+    CHECK_THAT(out, Equals("\xF8\xEF\x94"));
+  }
 }
 
-TEST_CASE( "float encoding" ) {
-    SECTION("floatp big-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::floatp<4, okser::end::be>>>(
-                okser::out::stdstring{out},
-                392.0853
-                );
-        CHECK_THAT(out, Equals("\x43\xC4\x0A\xEB"));
-    }
+TEST_CASE("float encoding") {
+  SECTION("floatp big-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::floatp<4, okser::end::be>>>(
+        okser::out::stdstring{out}, 392.0853);
+    CHECK_THAT(out, Equals("\x43\xC4\x0A\xEB"));
+  }
 
-    SECTION("floatp little-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::floatp<4, okser::end::le>>>(
-                okser::out::stdstring{out},
-                392.0853
-                );
-        CHECK_THAT(out, Equals("\xEB\x0A\xC4\x43"));
-    }
+  SECTION("floatp little-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::floatp<4, okser::end::le>>>(
+        okser::out::stdstring{out}, 392.0853);
+    CHECK_THAT(out, Equals("\xEB\x0A\xC4\x43"));
+  }
 
-    SECTION("doublep big-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::floatp<8, okser::end::be>>>(
-                okser::out::stdstring{out},
-                -1.9e158
-                );
-        CHECK_THAT(out, Equals("\xE0\xCB\xAD\x6C\x77\x40\x7F\x22"));
-    }
+  SECTION("doublep big-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::floatp<8, okser::end::be>>>(
+        okser::out::stdstring{out}, -1.9e158);
+    CHECK_THAT(out, Equals("\xE0\xCB\xAD\x6C\x77\x40\x7F\x22"));
+  }
 
-    SECTION("doublep little-endian") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::floatp<8, okser::end::le>>>(
-                okser::out::stdstring{out},
-                -1.9e158
-                );
-        CHECK_THAT(out, Equals("\x22\x7F\x40\x77\x6C\xAD\xCB\xE0"));
-    }
+  SECTION("doublep little-endian") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::floatp<8, okser::end::le>>>(
+        okser::out::stdstring{out}, -1.9e158);
+    CHECK_THAT(out, Equals("\x22\x7F\x40\x77\x6C\xAD\xCB\xE0"));
+  }
 }
 
-TEST_CASE( "enum encoding" ) {
-    enum class TestEnum : uint8_t {
-        A = 0x01,
-        B = 0x02,
-        C = 0x03,
-    };
+TEST_CASE("enum encoding") {
+  enum class TestEnum : uint8_t {
+    A = 0x01,
+    B = 0x02,
+    C = 0x03,
+  };
 
-    SECTION("8-bit enum to 8-bit value") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::enumv<TestEnum>>>(
-                okser::out::stdstring{out},
-                TestEnum::A
-                );
-        CHECK_THAT(out, Equals("\x01"));
-    }
+  SECTION("8-bit enum to 8-bit value") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::enumv<TestEnum>>>(
+        okser::out::stdstring{out}, TestEnum::A);
+    CHECK_THAT(out, Equals("\x01"));
+  }
 
-    SECTION("8-bit enum to 16-bit value") {
-        std::string out;
-        okser::serialize<okser::bundle<okser::enumv<TestEnum, 2>>>(
-                okser::out::stdstring{out},
-                TestEnum::B
-        );
-        CHECK(out[0] == 0x00);
-        CHECK(out[1] == 0x02);
-    }
-
+  SECTION("8-bit enum to 16-bit value") {
+    std::string out;
+    okser::serialize<okser::bundle<okser::enumv<TestEnum, 2>>>(
+        okser::out::stdstring{out}, TestEnum::B);
+    CHECK(out[0] == 0x00);
+    CHECK(out[1] == 0x02);
+  }
 }
