23e2b4ebbbedf2bb077b890c15fb88dfa0c9ed6f
kongr45gpen <electrovesta@gmail.com>
2023-05-15 14:42:07 +0200

    ci

--- test/source/optimizer_test.cpp
+++ test/source/optimizer_test.cpp
@@ -2,17 +2,17 @@
 
 uint8_t deserialize_uint8(std::string_view str) {
     auto number = okser::deserialize<okser::uint<1>, uint8_t, okser::in::range<std::string_view>>({str});
-    return number;
+    return *number;
 }
 
 uint8_t deserialize_uint32_be(std::string_view str) {
-    auto [number] = okser::deserialize<okser::bundle<okser::uint<4, okser::end::be>>, std::tuple<uint32_t>, okser::in::range<std::string_view>>(
+    auto number = okser::deserialize<okser::bundle<okser::uint<4, okser::end::be>>, std::tuple<uint32_t>, okser::in::range<std::string_view>>(
             {str});
-    return number;
+    return std::get<0>(*number);
 }
 
 uint8_t deserialize_uint32_le(std::string_view str) {
-    auto [number] = okser::deserialize<okser::bundle<okser::uint<4, okser::end::le>>, std::tuple<uint32_t>, okser::in::range<std::string_view>>(
+    auto number = okser::deserialize<okser::bundle<okser::uint<4, okser::end::le>>, std::tuple<uint32_t>, okser::in::range<std::string_view>>(
             {str});
-    return number;
+    return std::get<0>(*number);
 }
