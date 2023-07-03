a1ce8731792b9f09a2e70d48625f00a6953dc745
kongr45gpen <electrovesta@gmail.com>
2023-07-04 01:21:29 +0200

    Move reflection functionality in a separate file

--- example/reflection_example.cpp
+++ example/reflection_example.cpp
@@ -1,29 +1,10 @@
 #include <experimental/reflect>
 #include "mirror.hpp"
 #include "ok-serializer/ok-serializer.hpp"
+#include "ok-serializer/reflection.h"
 #include <concepts>
 
 #include <iostream>
-
-namespace okser {
-auto constexpr default_endianness = okser::end::be;
-
-template<class T>
-struct default_serializers;
-
-template<std::unsigned_integral T>
-struct default_serializers<T> {
-    using ser = okser::uint<sizeof(T), default_endianness>;
-    using deser = okser::uint<sizeof(T), default_endianness>;
-};
-
-template<std::signed_integral T>
-struct default_serializers<T> {
-    using ser = okser::sint<sizeof(T), default_endianness>;
-    using deser = okser::sint<sizeof(T), default_endianness>;
-};
-
-}
 
 struct Structure {
     int8_t a;
@@ -43,33 +24,10 @@ auto main() -> int {
     std::string result;
     okser::out::stdstring out(result);
 
-    for_each(get_data_members(mirror(Structure)), [&](auto member) {
-        std::cout << "Member:" << get_name(member) << "\t Type: " << get_name(get_type(member)) << std::endl;
-
-        const auto &value = get_value(member, s);
-        using type = std::remove_cvref_t<decltype(value)>;
-
-        using serializer = okser::default_serializers<type>::ser;
-
-        serializer::serialize(get_value(member, s), out);
-    });
+    okser::serialize_struct(out, s);
 
     std::cout << "Result: " << result << std::endl;
 
-    Structure s2;
-    okser::in::range in(result);
-
-    for_each(get_data_members(mirror(Structure)), [&](auto member) {
-        auto reference = get_reference(member, s2);
-        using type = std::remove_cvref_t<decltype(reference)>;
-
-        using deserializer = okser::default_serializers<type>::deser;
-
-        reference = *(okser::deserialize<deserializer>(result));
-
-        std::cout << "Member:" << get_name(member) << "\t Type: " << get_name(get_type(member)) << " \t Value: "
-                  << get_value(member, s) << std::endl;
-    });
-
+    okser::deserialize_struct<Structure>(okser::in::range(result));
 }
 
---
+++ include/ok-serializer/reflection.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <experimental/reflect>
+#include "../example/mirror.hpp"
+#include "ok-serializer/ok-serializer.hpp"
+#include <iostream>
+
+namespace okser {
+
+// TODO: Move these to a configuration class
+auto constexpr default_endianness = okser::end::be;
+
+template<class T>
+struct default_serializers;
+
+template<std::unsigned_integral T>
+struct default_serializers<T> {
+    using ser = okser::uint<sizeof(T), default_endianness>;
+    using deser = okser::uint<sizeof(T), default_endianness>;
+};
+
+template<std::signed_integral T>
+struct default_serializers<T> {
+    using ser = okser::sint<sizeof(T), default_endianness>;
+    using deser = okser::sint<sizeof(T), default_endianness>;
+};
+
+
+template<class T, Output Out>
+constexpr void serialize_struct(Out &&output, const T &object) {
+    auto mirrored_struct = mirror(T);
+
+    for_each(get_data_members(mirrored_struct), [&](auto member) {
+        std::cerr << "Member:" << get_name(member) << "\t Type: " << get_name(get_type(member)) << std::endl;
+
+        const auto &value = get_value(member, object);
+        using type = std::remove_cvref_t<decltype(value)>;
+
+        using serializer = default_serializers<type>::ser;
+
+        serializer::serialize(value, output);
+    });
+}
+
+template<class T, class In>
+constexpr T deserialize_struct(In &&input) {
+    // TODO: Find a more presentable and repeatable way to do this
+    auto contained_input = internal::convert_input_to_okser(std::forward<In>(input));
+
+    // TODO: Reuse functionality from bundle
+    std::optional<input_context<decltype(contained_input)>> context;
+    context.emplace(contained_input);
+
+    auto mirrored_struct = mirror(T);
+
+    T result; // TODO: There may be no default constructor
+
+    for_each(get_data_members(mirrored_struct), [&](auto member) {
+        auto &reference = get_reference(member, result);
+        using type = std::remove_cvref_t<decltype(reference)>;
+
+        using deserializer = default_serializers<type>::deser;
+
+        // TODO: Error handling
+        auto deserialized_result = deserializer::template deserialize<type>(*context);
+        reference = deserialized_result.first.value();
+        context.emplace(deserialized_result.second);
+
+        std::cout << "Member:" << get_name(member) << "\t Type: " << get_name(get_type(member))
+                  << " \t Value: "
+                  << get_value(member, result) << std::endl;
+    });
+
+    return result;
+}
+
+} // namespace okser
